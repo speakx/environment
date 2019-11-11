591dff2dd4f138521367ccc7164e979c1c51a704
jinjing <kimwoot2000@gmail.com>
2019-11-11 22:12:03 +0800

    mmapcache库拆分出来

--- src/mmapcache/mmapbytes.go
+++
@@ -1,7 +0,0 @@
-package mmapcache
-
-// MMapBlock x
-type MMapBlock struct {
-	DataTag uint16
-	Data    []byte
-}
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # 测试选项
 if [ ! -n "$1" ] ;then
-    echo "you need input test target { all | byteio | log | mmap | rocksdb }."
+    echo "you need input test target { all | log | rocksdb }."
     exit
 else
     echo "the test target is $1"
@@ -23,28 +23,11 @@ echo
 sh ./shell/gen-proto.sh
 sh ./shell/configure.sh
 
-# byteio test
-if [ "$target" == "all" ] || [ "$target" == "byteio" ] ;then
-    cd ./src
-    go test -v -bench=".*" ./byteio/byteio_test.go ./byteio/byteio.go
-    go test -bench=".*" ./byteio/byteio_test.go ./byteio/byteio.go
-    go test -v -bench=".*" ./byteio/mem_test.go ./byteio/mem.go
-    go test -bench=".*" ./byteio/mem_test.go ./byteio/mem.go
-fi
-
 # log test
 if [ "$target" == "all" ] || [ "$target" == "log" ] ;then
     cd ./src
     go test -v -bench=".*" ./logger/log_test.go ./logger/log.go
     rm -f ./logger/test.log*
-fi
-
-# mmap cache
-if [ "$target" == "all" ] || [ "$target" == "mmap" ] ;then
-    # go get github.com/edsrzf/mmap-go
-    cd ./src
-    go test -v ./mmapcache/mmapcache_test.go ./mmapcache/mmapcachepool.go ./mmapcache/mmapcache.go ./mmapcache/mmapdata.go
-    go test -bench=".*" ./mmapcache/mmapcache_test.go ./mmapcache/mmapcachepool.go ./mmapcache/mmapcache.go ./mmapcache/mmapdata.go
 fi
 
 # rocksdb test
